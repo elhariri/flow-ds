30ed5f34388149f7a3f1101e30424ac42f3cc10b
elhariri <abdelouahedelhariri@gmail.com>
2023-05-29 00:31:26 +0100

    slight adjustement to the mobile view

--- frontend/src/Components/Table/TableCells/ActionCell.tsx
+++ frontend/src/Components/Table/TableCells/ActionCell.tsx
@@ -3,20 +3,22 @@ import { TransactionType } from "../../../App.types";
 export default function ActionCell({ action }: { action: TransactionType }) {
   return (
     <td>
-      <span
-        style={{
-          backgroundColor: action === "ACHAT" ? "#ecfdf5" : "#fef2f2",
-          color: action === "ACHAT" ? "#10b981" : "#ef4444",
-          borderWidth: 1,
-          borderColor: action === "ACHAT" ? "#a7f3d0" : "#fecaca",
-        }}
-        className="rounded-full md:px-3 md:py-1 font-semibold text-[0.75rem]"
-      >
-        <span className="hidden md:inline-block">{action}</span>
-        <span className="md:hidden p-2 bg-transparent text-lg">
-          {action === "ACHAT" ? "+" : "-"}
-        </span>
-      </span>
+      <div className="flex">
+        <div
+          style={{
+            backgroundColor: action === "ACHAT" ? "#ecfdf5" : "#fef2f2",
+            color: action === "ACHAT" ? "#10b981" : "#ef4444",
+            borderWidth: 1,
+            borderColor: action === "ACHAT" ? "#a7f3d0" : "#fecaca",
+          }}
+          className="rounded-full m-auto md:px-3 md:py-1 w-fit h-fit font-semibold text-[0.75rem]"
+        >
+          <span className="hidden md:inline-block mx-auto">{action}</span>
+          <div className="inline-block md:hidden px-1 text-lg">
+            {action === "ACHAT" ? "+" : "-"}
+          </div>
+        </div>
+      </div>
     </td>
   );
 }
