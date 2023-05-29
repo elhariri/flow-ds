e193c960d35c73383d29680347adbd85a0548a39
elhariri <abdelouahedelhariri@gmail.com>
2023-05-29 11:08:44 +0100

    very small refactoring

--- backend/src/solution/Final/OutcomesGenerator/OutcomesGenerator.ts
+++ backend/src/solution/Final/OutcomesGenerator/OutcomesGenerator.ts
@@ -1,4 +1,3 @@
-import Config from "../../../config";
 import { Stocks } from "../../../index.types";
 import Portfolio from "../../Shared/Portfolio/Portfolio";
 import { Datapoint } from "../DataParser/DataParser.types";
@@ -6,80 +5,79 @@ import { Datapoint } from "../DataParser/DataParser.types";
 class OutcomesGenerator {
   private static sellStock(
     portfolio: Portfolio,
-    share: Stocks,
-    shareToBuy: Stocks,
-    date: number,
-    sellPrice: number,
-    buyPrice: number
-  ) {
-    portfolio.sellAllOf(share, sellPrice, date);
+    stock: Stocks,
 
-    portfolio.buyMaxShares(shareToBuy, buyPrice, date);
+    dataPoint: Datapoint
+  ) {
+    const { prices, date } = dataPoint;
+    const stockToBuy: Stocks = stock === "AMAZON" ? "GOOGLE" : "AMAZON";
+
+    portfolio.sellAllOf(stock, prices[stock].sellPrice, date);
+
+    portfolio.buyMaxShares(stockToBuy, prices[stockToBuy].buyPrice, date);
+  }
+
+  private static buyMaxSharesAndPushOutcome(
+    portfolio: Portfolio,
+    stock: Stocks,
+    dataPoint: Datapoint,
+    newPortfolios: Portfolio[]
+  ) {
+    const outcomePortfolio = portfolio.clone();
+    const { prices, date } = dataPoint;
+
+    const boughtStock = outcomePortfolio.buyMaxShares(
+      stock,
+      prices[stock].buyPrice,
+      date
+    );
+
+    if (boughtStock) {
+      newPortfolios.push(outcomePortfolio);
+    }
+  }
+
+  private static handleBuyScenario(
+    portfolio: Portfolio,
+    dataPoint: Datapoint,
+    newPortfolios: Portfolio[]
+  ) {
+    this.buyMaxSharesAndPushOutcome(
+      portfolio,
+      "AMAZON",
+      dataPoint,
+      newPortfolios
+    );
+    this.buyMaxSharesAndPushOutcome(
+      portfolio,
+      "GOOGLE",
+      dataPoint,
+      newPortfolios
+    );
+  }
+
+  private static handleSellScenario(
+    portfolio: Portfolio,
+    dataPoint: Datapoint,
+    newPortfolios: Portfolio[]
+  ) {
+    const stock = portfolio.amazonShares > 0 ? "AMAZON" : "GOOGLE";
+
+    this.sellStock(portfolio, stock, dataPoint);
+
+    newPortfolios.push(portfolio);
   }
 
   static from(portfolios: Portfolio[], dataPoint: Datapoint) {
     const newPortfolios: Portfolio[] = [];
 
-    const { date } = dataPoint;
-
     for (let j = 0; j < portfolios.length; j += 1) {
       const portfolio = portfolios[j].clone();
 
-      let portfolioTotalShares = 0;
-
-      for (let i = 0; i < Config.Stocks.length; i += 1) {
-        const stock = Config.Stocks[i];
-
-        portfolioTotalShares += portfolio.totalShares[stock];
-      }
-
-      if (portfolioTotalShares > 0) {
-        // buy stock and create an outcome for each one of them
-        for (let i = 0; i < Config.Stocks.length; i += 1) {
-          const stock = Config.Stocks[i];
-
-          if (portfolio.totalShares[stock] > 0) {
-            const { sellPrice } = dataPoint.prices[stock];
-
-            const leftStocks = Config.Stocks.filter(
-              (currStock) => currStock !== stock
-            );
-
-            // ceate an outcome for each stock that can be bought with the money
-            for (let k = 0; k < leftStocks.length; k += 1) {
-              const leftStock = leftStocks[k];
-              const stockPortfolio = portfolio.clone();
-
-              this.sellStock(
-                stockPortfolio,
-                stock,
-                leftStock,
-                date,
-                sellPrice,
-                dataPoint.prices[leftStock].buyPrice
-              );
-
-              newPortfolios.push(stockPortfolio);
-            }
-          }
-        }
+      if (portfolio.googleShares + portfolio.amazonShares > 0) {
+        this.handleSellScenario(portfolio, dataPoint, newPortfolios);
       } else {
-        // if no stocks are owned, create an outcome for buying each stock
-        for (let i = 0; i < Config.Stocks.length; i += 1) {
-          const stock = Config.Stocks[i];
-          const stockPortfolio = portfolio.clone();
-          const { buyPrice } = dataPoint.prices[stock];
-
-          const boughtStock = stockPortfolio.buyMaxShares(
-            stock,
-            buyPrice,
-            date
-          );
-
-          if (boughtStock) {
-            newPortfolios.push(stockPortfolio);
-          }
-        }
+        this.handleBuyScenario(portfolio, dataPoint, newPortfolios);
       }
     }
 
